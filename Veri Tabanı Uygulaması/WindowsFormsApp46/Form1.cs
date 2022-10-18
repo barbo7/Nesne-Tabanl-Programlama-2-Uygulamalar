e09da35d3848f308121af32d166187c64ba3aff2
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-10-18 21:53:00 +0300

    Add files via upload
    Ders notları

---
+++ Veri Tabanı Uygulaması/WindowsFormsApp46/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp46
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=LENOVO\\SQLEXPRESS;Initial Catalog=aaa;Integrated Security=True"); //Database'in bulunduğu yer
+        BindingSource bs = new BindingSource(); //Daha hızlı veri akışı için kullandığımız tool
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM kimlik", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            bindingSource1.DataSource = dt;
+
+            dataGridView1.DataSource = bindingSource1;
+            textBox1.DataBindings.Add("Text", bindingSource1,"okulno");
+            textBox2.DataBindings.Add("Text", bindingSource1, "Ad");
+            textBox3.DataBindings.Add("Text", bindingSource1, "Soyad");
+            textBox4.DataBindings.Add("Text", bindingSource1, "Tc");
+            textBox5.DataBindings.Add("Text", bindingSource1, "Adres");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bindingSource1.AddNew(); // By işlem sadece form üzeirndeki nesnelerde yeni kayıt alanı açmak için kullanılır. 
+            textBox1.Focus();                        //Asla veri tabanına müdale değildir.
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bindingSource1.EndEdit(); //Yeni açılan nesnelere girilen kayıtların form üzerindeki datagrid nesnesine akarmk için kullanılır.
+            bindingSource1.AddNew(); //Bu şekilde kullanım bizi yanıltabilir sebebi ise ikinci bir komut kullanmamızı bekler.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveFirst(); //İlk kayıt'a gitmek için kullanılır.
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MovePrevious(); //Önceki kayıt'a gitmek için kullanılır.
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveLast(); //Son kayıt'a gitmek için kullanılır.
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveNext(); //Sonraki kayıt'a gitmek için kullanılır.
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {   
+            con.Open();
+            SqlCommand kaydet = new SqlCommand("Insert into kimlik(Ad,Soyad,Tc,Adres,Sınıf,Şube,TelNo) values('" + textBox2.Text + "','" + textBox3.Text + "'," + textBox4.Text + ",'" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7 + "'," + textBox8.Text + ")",con);
+            kaydet.ExecuteNonQuery();
+            con.Close();
+
+        }
+        // bu işlemlerin hiçbirisi veritabanı işlemi değildir. Bunların hepsi sadece sanal kayıt'ta gösterilen olaylardır.
+        //Girilen kayıtların veritabanına işlenebilmesi için mutlaka insert komutu kullanılmalıdır.
+    }
+}
---
+++ Veri Tabanı Uygulaması/WindowsFormsApp46/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("WindowsFormsApp46")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp46")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("295af0d1-fb0b-488e-b909-f46b73e88515")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
