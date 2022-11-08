ff5c2476eb48e42fcf2342950e4bc7a34ed8f132
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-11-08 23:18:38 +0300

    Add files via upload
    Ders uygulamaları eklendy

---
+++ HashTable ile Sql Uygulamaları/WindowsFormsApp51/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp51
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(434, 258);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(529, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 318);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HashTable ile Sql Uygulamaları/WindowsFormsApp51/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp51
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=LENOVO\\SQLEXPRESS;Initial Catalog=Yeni;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hashtable deneme = new Hashtable();
+            int n = int.Parse(Interaction.InputBox("Kaç defa veri girileceğini yazınız!!", ""));
+
+            do
+            {
+                string key = Interaction.InputBox("Key değerini giriniz(int):");
+                string val = Interaction.InputBox("Value değerini giriniz:");
+
+                con.Open();
+                cmd = new SqlCommand("Insert Into tablo(okul_no,ad_soyad)  values(" + key + ",'" + val + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                deneme[key] = val;
+                
+            }
+            while (n-- > 1);
+            kaydet();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kaydet();
+        }
+        private void kaydet()
+        {
+            con.Open();
+            cmd = new SqlCommand("Select * from tablo", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+        }
+    }
+}
