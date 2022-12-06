a78d5539bd9140e7e4e728563a7c127e4e6d1200
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-12-06 19:24:07 +0300

    Add files via upload

---
+++ Thread devam/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(259, 22);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(224, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(513, 22);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(224, 238);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(771, 22);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(224, 238);
+            this.listBox4.TabIndex = 3;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(1032, 22);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(224, 238);
+            this.listBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(762, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1105, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 341);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Thread devam/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        Thread islem1;
+        Thread islem2;
+        Thread islem3;
+        Thread islem4;
+        Thread islem5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int m1 = 1, m2_1 = 2021, m2_2 = 707001;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1.CheckForIllegalCrossThreadCalls = false;
+
+            islem1 = new Thread(new ThreadStart(metot1));
+            islem2 = new Thread(new ThreadStart(metot2));
+            islem3 = new Thread(new ThreadStart(metot3));
+            islem4 = new Thread(new ThreadStart(metot4));
+            islem5 = new Thread(new ThreadStart(metot5));
+
+            islem1.Start();
+            islem2.Start();
+            islem3.Start();
+            islem4.Start();
+            islem5.Start();
+        }
+        void metot1()
+        {
+            while(true)
+            {
+                listBox1.Items.Add(m1++);
+                Thread.Sleep(1000);
+            }
+        }
+        void metot2()
+        {
+            while (true)
+            {
+                listBox2.Items.Add(m2_2++);
+                Thread.Sleep(1000);
+                if (m2_2 + m2_1 >= 999999)
+                {
+                    islem1.Abort();
+                    islem2.Abort();
+                    islem3.Abort();
+                    islem4.Abort();
+                    islem5.Abort();
+                }
+                    
+            }
+        }
+        void metot3()
+        {
+            List<string> program = new List<string>{"a","b","c","d","e","f","g","h","l","m","n"};
+            string[,] programkopya = new String[10, 2] { { "a", "" }, };
+            int i=9;
+            Random rnd = new Random();
+
+            if (i < 0) islem3.Abort();
+
+            while(true)
+            {
+                int rast = rnd.Next(i);
+                listBox3.Items.Add(program[rast]);
+                Thread.Sleep(1000);
+                program.RemoveAt(rast);
+                i--;
+            }
+        }
+        void metot4()
+        {
+            //metot3te devam et.
+
+        }
+        void metot5()
+        {
+            List<int> seksenbir = new List<int>();
+            for (int i = 2; i < 81; i++)
+                seksenbir.Add(i);
+            Random rnd = new Random();
+            while(true)
+            {
+                int rast = rnd.Next(seksenbir.Count);
+                listBox5.Items.Add(rast);
+                seksenbir.Remove(rast);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            islem1.Abort();
+            islem2.Abort();
+            islem3.Abort();
+            islem4.Abort();
+            islem5.Abort();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
