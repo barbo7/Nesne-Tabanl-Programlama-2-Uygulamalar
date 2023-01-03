99a7a7d6b65c53263e0ceb3edd6da9b98c2d7259
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2023-01-03 19:07:34 +0300

    Add files via upload

---
+++ Emrah Prog/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtOkulNo = new System.Windows.Forms.TextBox();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtBasariNotu = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtDersAdi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYAD :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "OKUL NO :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TC : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "DERS ADI :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "VİZE : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(67, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "FİNAL : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "BAŞARI NOTU :";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(121, 19);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(152, 20);
+            this.txtAd.TabIndex = 8;
+            this.txtAd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(121, 44);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(152, 20);
+            this.txtSoyad.TabIndex = 9;
+            this.txtSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtOkulNo
+            // 
+            this.txtOkulNo.Location = new System.Drawing.Point(121, 71);
+            this.txtOkulNo.Name = "txtOkulNo";
+            this.txtOkulNo.Size = new System.Drawing.Size(152, 20);
+            this.txtOkulNo.TabIndex = 10;
+            this.txtOkulNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(121, 100);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(152, 20);
+            this.txtTC.TabIndex = 11;
+            this.txtTC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtBasariNotu
+            // 
+            this.txtBasariNotu.Enabled = false;
+            this.txtBasariNotu.Location = new System.Drawing.Point(121, 211);
+            this.txtBasariNotu.Name = "txtBasariNotu";
+            this.txtBasariNotu.Size = new System.Drawing.Size(152, 20);
+            this.txtBasariNotu.TabIndex = 15;
+            this.txtBasariNotu.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(121, 182);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(152, 20);
+            this.txtFinal.TabIndex = 14;
+            this.txtFinal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(121, 155);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(152, 20);
+            this.txtVize.TabIndex = 13;
+            this.txtVize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // txtDersAdi
+            // 
+            this.txtDersAdi.Location = new System.Drawing.Point(121, 130);
+            this.txtDersAdi.Name = "txtDersAdi";
+            this.txtDersAdi.Size = new System.Drawing.Size(152, 20);
+            this.txtDersAdi.TabIndex = 12;
+            this.txtDersAdi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 42);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 42);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 42);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(404, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(490, 208);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 314);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBasariNotu);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.txtDersAdi);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.txtOkulNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtOkulNo;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtBasariNotu;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtDersAdi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Emrah Prog/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+using ClassLibrary1;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection bgl = new SqlConnection("Data Source=DESKTOP-SRT1JQQ;Initial Catalog=OGRENCI;Integrated Security=True");
+        Hesapla hesap = new Hesapla();
+
+        string sifre;
+        byte sayac = 1, yanlis = 0;
+        string ogrsayi;
+
+        private void Listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from TBLOGRENCI", bgl);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Temizle()
+        {
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtBasariNotu.Clear();
+            txtDersAdi.Clear();
+            txtFinal.Clear();
+            txtOkulNo.Clear();
+            txtTC.Clear();
+            txtVize.Clear();
+            txtAd.Select();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            byte[] sayi = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 1; i <= 11; i++)
+            {
+                sifre += sayi[rnd.Next(0, 9)].ToString();
+            }
+            bgl.Open();
+
+            SqlCommand sifreEkle = new SqlCommand("insert into TBLSIFRE (SIFRE) values ('" + sifre + "')", bgl);
+            sifreEkle.ExecuteNonQuery();
+            Registry.CurrentUser.CreateSubKey("Parola").SetValue("Parola", sifre);
+
+            bgl.Close();
+            MessageBox.Show("Şifreniz : " + sifre);
+            try 
+	        {
+                ogrsayi = Microsoft.VisualBasic.Interaction.InputBox("Kaç öğrenci gireceksiniz?", "Girilecek Öğrenci Sayısı", "");
+                int result;
+                if (int.TryParse(ogrsayi, out result))
+                {
+                    if (Convert.ToByte(ogrsayi) > 0 & Convert.ToByte(ogrsayi) < 250)
+                        txtAd.Enabled = true;
+                    else
+                    {
+                        MessageBox.Show("Hatalı Giriş.");
+                        Application.Restart();
+                    } 
+                }
+                else
+                {
+                    MessageBox.Show("Girdiğiniz Karakter Sayısal Değil !!!");
+                    Application.Restart();
+                }
+	        }
+	        catch (Exception)
+	        {
+    
+	        }
+            Listele();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Microsoft.VisualBasic.Interaction.InputBox("Şifre giriniz", "Şifre", "") == Registry.CurrentUser.OpenSubKey("Parola").GetValue("Parola").ToString())
+            {
+                if (sayac <= Convert.ToByte(ogrsayi))
+                {
+                    if ((txtAd.Text.Length <= 30 & txtAd.Text.Length > 0) & (txtSoyad.Text.Length <= 30 & txtSoyad.Text.Length > 0) & (txtOkulNo.Text.Length <= 10 & txtOkulNo.Text.Length > 0) & (txtTC.Text.Length <= 11 & txtTC.Text.Length > 0) & (txtDersAdi.Text.Length <= 30 & txtDersAdi.Text.Length > 0) & (Convert.ToByte(txtVize.Text) <= 100 & Convert.ToByte(txtVize.Text) >= 0) & (Convert.ToByte(txtFinal.Text) <= 100 & Convert.ToByte(txtFinal.Text) >= 0))
+                    {
+                        bgl.Open();
+                        SqlCommand ekle = new SqlCommand("insert into TBLOGRENCI (AD, SOYAD, OKULNO, TC, DERSAD, VIZE, FINAL, BASARINOTU) values ('" + txtAd.Text + "','" + txtSoyad.Text + "','" + txtOkulNo.Text + "','" + txtTC.Text + "','" + txtDersAdi.Text + "','" + txtVize.Text + "','" + txtFinal.Text + "','" + hesap.BasariHesapla(Convert.ToByte(txtVize.Text), Convert.ToByte(txtFinal.Text)) + "')", bgl);
+                        txtBasariNotu.Text = hesap.BasariHesapla(Convert.ToByte(txtVize.Text), Convert.ToByte(txtFinal.Text)).ToString();
+                        ekle.ExecuteNonQuery();
+                        sayac++;
+                        bgl.Close();
+                        Listele();
+                        Temizle(); 
+                    }
+                    else
+                        MessageBox.Show("Ad, soyad ve ders adı 30 karakter olmalı, okul no 10, TC 11 vize ve final 0 ve 100 arasında olmalı");
+                }
+                else
+                {
+                    MessageBox.Show("Daha fazla öğrenci girişi yapamazsınız.");
+                    Temizle();
+                }
+            }
+            else
+            {
+                yanlis++;
+                MessageBox.Show("Şifre Yanlış");
+                if (yanlis == 3)
+                {
+                    bgl.Open();
+                    SqlCommand sil = new SqlCommand("delete from TBLOGRENCI", bgl);
+                    sil.ExecuteNonQuery();
+                    bgl.Close();
+                    MessageBox.Show("Tüm Veriler Silindi");
+                    Listele();
+                    Temizle();
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtAd.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtSoyad.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtOkulNo.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtTC.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtDersAdi.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtVize.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+                txtFinal.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+                txtBasariNotu.Text = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Microsoft.VisualBasic.Interaction.InputBox("Şifre giriniz", "Şifre", "") == Registry.CurrentUser.OpenSubKey("Parola").GetValue("Parola").ToString())
+            {
+                if (txtAd.Text.Length <= 30 & txtAd.Text.Length > 0 & txtSoyad.Text.Length <= 30 & txtSoyad.Text.Length > 0 & txtOkulNo.Text.Length <= 10 & txtOkulNo.Text.Length > 0 & txtTC.Text.Length <= 11 & txtTC.Text.Length > 0 & txtDersAdi.Text.Length <= 30 & txtDersAdi.Text.Length > 0 & Convert.ToByte(txtVize.Text) <= 100 & Convert.ToByte(txtVize.Text) >= 0 & Convert.ToByte(txtFinal.Text) <= 100 & Convert.ToByte(txtFinal.Text) >= 0)
+                {
+                    bgl.Open();
+                    SqlCommand guncelle = new SqlCommand("update TBLOGRENCI set AD= '" + txtAd.Text + "' , SOYAD = '" + txtSoyad.Text + "', OKULNO = '" + txtOkulNo.Text + "', TC= '" + txtTC.Text + "' , DERSAD = '" + txtDersAdi.Text + "', VIZE = '" + txtVize.Text + "', FINAL = '" + txtFinal.Text + "', BASARINOTU = '" + hesap.BasariHesapla(Convert.ToByte(txtVize.Text), Convert.ToByte(txtFinal.Text)) + "' where TC = '" + txtTC.Text + "' ", bgl);
+                    guncelle.ExecuteNonQuery();
+                    MessageBox.Show("İşlem tamamlandı");
+                    Temizle();
+                    Listele();
+                    bgl.Close();
+                }
+                else
+                    MessageBox.Show("Ad, soyad ve ders adı 30 karakter olmalı, okul no 10, TC 11 vize ve final 0 ve 100 arasında olmalı");
+            }
+            else
+            {
+                yanlis++;
+                MessageBox.Show("Şifre Yanlış");
+                if (yanlis == 3)
+                {
+                    bgl.Open();
+                    SqlCommand sil = new SqlCommand("delete from TBLOGRENCI", bgl);
+                    sil.ExecuteNonQuery();
+                    bgl.Close();
+                    MessageBox.Show("Tüm Veriler Silindi");
+                    Listele();
+                    Temizle();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Microsoft.VisualBasic.Interaction.InputBox("Şifre giriniz", "Şifre", "") == Registry.CurrentUser.OpenSubKey("Parola").GetValue("Parola").ToString())
+            {
+                bgl.Open();
+                SqlCommand sil = new SqlCommand("delete from TBLOGRENCI where TC = '" + txtTC.Text + "' ", bgl);
+                sil.ExecuteNonQuery();
+                MessageBox.Show("İşlem tamamlandı");
+                Temizle();
+                Listele();
+                bgl.Close();
+            }
+            else
+            {
+                yanlis++;
+                MessageBox.Show("Şifre Yanlış");
+                if (yanlis == 3)
+                {
+                    bgl.Open();
+                    SqlCommand sil = new SqlCommand("delete from TBLOGRENCI", bgl);
+                    sil.ExecuteNonQuery();
+                    bgl.Close();
+                    MessageBox.Show("Tüm Veriler Silindi");
+                    Listele();
+                    Temizle();
+                }
+            }
+        }
+    }
+}
